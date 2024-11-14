56d095d62781ebc4b71885986d1412357e1a92c7
Flourez Alexis <flourez.alexis@gmail.com>
2024-11-14 07:53:06 +0100

    Test slide tel

--- src/components/Slide/Slide.js
+++ src/components/Slide/Slide.js
@@ -1,18 +1,47 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Slide.scss';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faGithubSquare } from '@fortawesome/free-brands-svg-icons';
 
 const Slide = ({ image, title, description, lienGitHub, onPrev, onNext }) => {
+  // Utiliser les positions de départ et de fin du touché
+  const [touchStartX, setTouchStartX] = useState(0);
+  const [touchEndX, setTouchEndX] = useState(0);
+
+  // Commence le swipe
+  const handleTouchStart = (e) => {
+    setTouchStartX(e.targetTouches[0].clientX);
+  };
+
+  // Fin du swipe
+  const handleTouchMove = (e) => {
+    setTouchEndX(e.targetTouches[0].clientX);
+  };
+
+  // Gère la direction du swipe
+  const handleTouchEnd = () => {
+    if (touchStartX - touchEndX > 50) {
+      onNext(); // Swipe vers la gauche, aller au suivant
+    }
+    if (touchStartX - touchEndX < -50) {
+      onPrev(); // Swipe vers la droite, aller au précédent
+    }
+  };
+
   return (
-    <div className="slide">
+    <div
+      className="slide"
+      onTouchStart={handleTouchStart}
+      onTouchMove={handleTouchMove}
+      onTouchEnd={handleTouchEnd}
+    >
       <div className="image-container">
         <img src={image} alt={`Image du projet ${title}`} className="slide-image" />
       </div>
       <div className="text-container">
         <h3>{title}</h3>
         <p className="slide-description">{description}</p>
-        <a href={lienGitHub} className="slide-icon-github" target='_blank' rel="noreferrer">
+        <a href={lienGitHub} className="slide-icon-github" target="_blank" rel="noreferrer">
           <FontAwesomeIcon icon={faGithubSquare} />
         </a>
       </div>
